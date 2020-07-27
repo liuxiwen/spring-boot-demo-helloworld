36cfde97833c38a970e956bf444efa45d2fd5616
liuxiwen <xiwenhehe@sina.com>
2020-07-27 21:21:25 +0800

    feat(*): spring-boot-demo-helloworld初始化完成

---
+++ src/test/java/com/xkcoding/helloworld/SpringBootDemoHelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xkcoding.helloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDemoHelloworldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
